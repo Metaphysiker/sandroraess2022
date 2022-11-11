7ef3ed8f51fa969d76bd57b2d8cef864bcbe19dd
Metaphysiker <s.raess@me.com>
2022-11-11 20:23:06 +0100

    added bootstrap

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Sandroraess2022
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    I18n.available_locales = [:de]
+    config.i18n.default_locale = :de
 
     # Configuration for the application, engines, and railties goes here.
     #
