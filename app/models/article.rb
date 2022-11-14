005e02b961ff83bda712f5a82c4e9ee6b5fd8b44
Metaphysiker <s.raess@me.com>
2022-11-14 22:34:50 +0100

    working on nokogiri

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,9 +11,9 @@ class Article < ApplicationRecord
 
   def modify_content
 
-    #h1 = Tree::TreeNode.new('h1', self.title)
+    root = Tree::TreeNode.new('root', self.title)
 
-    #h1 << Tree::TreeNode.new("CHILD2", "Child2 Content")
+    #h2 << Tree::TreeNode.new("CHILD2", "Child2 Content")
 
     #h1.print_tree
 
@@ -24,15 +24,48 @@ class Article < ApplicationRecord
     doc = Nokogiri::HTML.fragment(self.content)
     table_of_content = ""
 
+    current_heading_level = 1
+    current_node = root
+    last_node = root
+
     doc.css("h1, h2, h3, h4, h5, h6").each do |node|
+
+      heading_level = node.name.delete("^0-9").to_i
+      puts heading_level
+
+      #if heading_level is the same as current_level add to children of last node
+      if heading_level == current_heading_level
+        new_node = Tree::TreeNode.new(node.content, node.content)
+        last_node << new_node
+        #current_node = new_node
+        #last_node = new_node
+        #current_heading_level = heading_level
+
+      #
+      elsif heading_level < current_heading_level
+        new_node = Tree::TreeNode.new(node.content, node.content)
+
+        last_node << new_node
+        current_node = new_node
+
+        current_heading_level = heading_level
+
+      else
+        new_node = Tree::TreeNode.new(node.content, node.content)
+
+        current_node = root
+        current_node << new_node
+        current_heading_level = heading_level
+      end
+
 
       times_of_space = node.name.delete("^0-9").to_i
       puts "node"
       #puts node.attributes
       #puts node.methods
-      puts node.name
-      puts node.children.count
-      puts node.children[0].content
+      #puts node.name
+      #puts node.children.count
+      #puts node.children[0].content
       node['id'] = node.content.parameterize
 
       times_of_space_string = ""
@@ -49,6 +82,8 @@ EOF
 
     #https://developer.squareup.com/blog/challenge-table-of-contents-generator/
     self.modified_content = table_of_content + "<hr>" + doc.to_html
+
+    root.print_tree
   end
 
 end
