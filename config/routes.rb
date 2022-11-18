72d5ae2fbc5c0d2411c610db6787340277b149b4
Metaphysiker <s.raess@me.com>
2022-11-18 06:43:23 +0100

    added translations for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   localized do
     resources :articles
+    get 'about_me', to: "static_pages#about_me", as: "about_me"
+    get 'contact', to: "static_pages#contact", as: "contact"
+    get 'support', to: "static_pages#support", as: "support"
   end
   get 'static_pages/welcome'
-  get 'about_me', to: "static_pages#about_me", as: "about_me"
-  get 'contact', to: "static_pages#contact", as: "contact"
-  get 'support', to: "static_pages#support", as: "support"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
