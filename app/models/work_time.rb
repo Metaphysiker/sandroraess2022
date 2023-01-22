dad1b8117a4c9e95066874f96a2c107c5fdb80fd
Metaphysiker <s.raess@me.com>
2023-01-23 00:08:51 +0100

    before merge

--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -13,7 +13,7 @@ class WorkTime < ApplicationRecord
     elsif work_times_with_global_work_time_ids.count == 1
       work_time = work_times_with_global_work_time_ids.first
 
-      if work_time.updated_at < work_time_params["updated_at"]
+      if work_time.updated_at < work_time_params["updated_at"].to_datetime
         work_time.update(work_time_params)
       end
 
@@ -26,7 +26,7 @@ class WorkTime < ApplicationRecord
 
       WorkTime.where(id: work_time_ids_to_be_deleted).delete_all
 
-      if work_time.updated_at < work_time_params["updated_at"]
+      if work_time.updated_at < work_time_params["updated_at"].to_datetime
         work_time.update(work_time_params)
       end
 
